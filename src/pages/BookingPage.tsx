313abd42832bacaa87aace8d9975f7781a90d61d
RSRDEV <rianflamengo8@gmail.com>
2025-11-16 21:25:01 -0300

    Adicionar 08:00 após 07:00 na lista de horários

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -60,7 +60,7 @@ export function BookingPage({ onNavigate }: BookingPageProps) {
   };
 
   const timeSlots = [
-    '07:00', '09:00', '10:00', '11:00',
+    '07:00', '08:00', '09:00', '10:00', '11:00',
     '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'
   ];
 
